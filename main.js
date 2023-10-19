1a070f9a63d166fc58dc15c5571bdb5d11d42ad3
harishskcrp <148299687+harishskcrp@users.noreply.github.com>
2023-10-19 19:33:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
 const assert = require('assert');
 
 function countBatteriesByHealth(presentCapacities) {
-  return {
+  const counts = {
     healthy: 0,
     exchange: 0,
     failed: 0
   };
+
+  for (const capacity of presentCapacities) {
+     const ratedCapacity = 120;  
+    const soh = (capacity / ratedCapacity) * 100;
+
+     if (soh > 80) {
+      counts.healthy++;
+    } else if (soh >= 63 && soh <= 80) {
+      counts.exchange++;
+    } else {
+      counts.failed++;
+    }
+  }
+
+  return counts;
 }
 
 function testBucketingByHealth() {
   console.log('Counting batteries by SoH...');
   const presentCapacities = [113, 116, 80, 95, 92, 70];
-  counts = countBatteriesByHealth(presentCapacities);
-  assert(counts["healthy"] == 2);
-  assert(counts["exchange"] == 3);
-  assert(counts["failed"] == 1);
+  const counts = countBatteriesByHealth(presentCapacities);
+  assert.strictEqual(counts.healthy, 2);
+  assert.strictEqual(counts.exchange, 3);
+  assert.strictEqual(counts.failed, 1);
   console.log("Done counting :)");
 }
 
 testBucketingByHealth();
+
